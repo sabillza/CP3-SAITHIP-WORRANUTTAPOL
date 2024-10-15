79768c09169c02bc92f4ef46ad1095f354289e26
sabillza <164137753+sabillza@users.noreply.github.com>
2024-10-15 12:53:27 +0700

    Update Exercise5_1_SAITHIP_W.py

--- Myassignment /Exercise5_1_SAITHIP_W.py
+++ Myassignment /Exercise5_1_SAITHIP_W.py
@@ -4,3 +4,7 @@ addition = num1 + num2
 subtraction = num1 - num2
 multiplication = num1 * num2
 division = num1 / num2
+print(f"{num1} + {num2} = {addition}")
+print(f"{num1} - {num2} = {subtraction}")
+print(f"{num1} * {num2} = {multiplication}")
+print(f"{num1} / {num2} = {division}")
